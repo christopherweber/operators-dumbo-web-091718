c25977f2ab19892024118a8b3b40c09311958418
Christopher Weber <chrisrrweber@gmail.com>
2018-08-15 23:41:46 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -12,3 +12,10 @@ def not_safe?(speed)
 	(speed < 40 || speed > 60) ? true (40..60) : false
 end
 
+
+# //puts (if 1 then 2 else 3 end) # => 2
+
+# puts 1 ? 2 : 3                # => 2
+
+# x = if 1 then 2 else 3 end
+# puts x                        # => 2
