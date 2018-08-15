e1dee503a02831a6d78bd96cb68abc91151c155b
Christopher Weber <chrisrrweber@gmail.com>
2018-08-15 23:25:06 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,16 @@
-# def unsafe?(speed)
-#   if speed == (40..60)
-#     return false
-#   else speed == (speed < 40 && speed > 60)
-#     return true
-#   end
-# end
+def unsafe?(speed)
+  if speed == (40..60)
+    return false
+  else speed == (speed < 40 && speed > 60)
+    return true
+  end
+end
 
 
 
 def not_safe?(speed)
 	speed == (40..60) ? true : false
+	speed < 40 %% > 60 ? true : false
 end
 	
 
